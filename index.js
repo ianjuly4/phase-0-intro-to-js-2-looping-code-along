95f4449700ac47f542c64c3e5f3b139f36b14e78
Ian July <ianjuly4@gmail.com>
2023-07-22 12:21:27 -0600

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+//const cards = ["Guadalupe,", "Ollie,", "Aki,"];
+
+function writeCards(cards) {
+  let cardsWrite = [];
+  for (let i = 0; i < cards.length; i++) {
+    cardsWrite.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    console.log(cardsWrite);
+  }
+
+  return cardsWrite;
+}
+writeCards(["mike", "tom", "bill"]);
+
+
+function countDown(number){
+  while (number > -1) {
+  console.log(number --);
+}
+}
+countDown(5)
